ff29f0722e51ce88971c65d52157d8ae723216bf
sshJerry <defineantoine@gmail.com>
2021-05-03 11:53:23 -0400

    mess

--- main.py
+++ main.py
@@ -2,24 +2,31 @@ import curses
 # import urwid
 import time
 
-menu = ['Login', 'Unclassified', 'Exit']
+menuGuest = ['Login', 'Unclassified', 'Exit']
+menuUser = ['See Medium Level Files', 'Unclassified', 'Exit']
+menuAdmin = ['See Admin Level Files', menuUser[0], menuUser[1], menuUser[2]]
 
 
 # cd Downloads/VS\ CODE/pythonProject/ || python3 main.py
 
 class User:
-    def __init__(self, level, username, password):
+    def __init__(self, level, username, password, permission):
         self.level = level
         self.username = username
         self.password = password
+        self.permission = permission
+
+    def get_username(self):
+        return self._username
 
 
-def print_menu(stdscr, selected_option):
+def print_menu(stdscr, selected_option, access_request, name):
     stdscr.clear()
     h, w = stdscr.getmaxyx()
-    for i, row in enumerate(menu):
+    stdscr.addstr(0, 0, ("Currently: " + str(name)))
+    for i, row in enumerate(access_request):
         x = w // 2 - len(row) // 2
-        y = h // 2 - len(menu) // 2 + i
+        y = h // 2 - len(access_request) // 2 + i
         if i == selected_option:
             stdscr.attron(curses.A_REVERSE)
             stdscr.addstr(y, x, row)
@@ -29,7 +36,7 @@ def print_menu(stdscr, selected_option):
     stdscr.refresh()
 
 
-def print_login(stdscr):
+def print_login(stdscr, u1, p1, u2, p2):
     stdscr.clear()
     curses.echo()
     stdscr.addstr(0, 0, "Username: ")
@@ -37,7 +44,25 @@ def print_login(stdscr):
     stdscr.refresh()
     username = stdscr.getstr(0, 10, 15)
     password = stdscr.getstr(1, 10, 15)
-    pass
+    str(username)
+    str(password)
+    stdscr.addstr(5, 5, username)
+    stdscr.addstr(6, 5, u1)
+    stdscr.addstr(8, 5, password)
+    stdscr.addstr(9, 5, p1)
+
+    stdscr.addstr(5, 15, username)
+    stdscr.addstr(6, 15, u2)
+    stdscr.addstr(8, 15, password)
+    stdscr.addstr(9, 15, p2)
+    stdscr.refresh()
+    if (str(username) == str(u1)) and (str(password) == str(p1)):
+        return str(u1)
+    elif username == u2:
+        if password == p2:
+            return u2.username
+    else:
+        return 'guest'
 
 
 def unclassified_view(stdscr):
@@ -45,13 +70,13 @@ def unclassified_view(stdscr):
 
 
 def main(stdscr):
-    admin = User('Top', 'admin', 'password1')
-    worker = User('Medium', 'user', 'password2')
-    guest = User('Bottom', 'guest', 'password3')
+    admin = User('Top', 'admin', '1', menuAdmin)
+    worker = User('Medium', 'user', 'password2', menuUser)
+    guest = User('Bottom', 'guest', '', menuGuest)
     curses.curs_set(0)
 
     highlight = 0
-    print_menu(stdscr, highlight)
+    print_menu(stdscr, highlight, guest.permission, guest.username)
 
     # stdscr.clear()
     while True:
@@ -63,15 +88,14 @@ def main(stdscr):
             highlight += 1
         elif key == curses.KEY_ENTER or key in [10, 13]:
             if highlight == 0:
-                print_login(stdscr)
-            # curses.nocbreak()
-            # stdscr.keypad(False)
-            # curses.echo()
+                access = print_login(stdscr, admin.username, admin.password, worker.username, worker.password)
+                stdscr.addstr(3, 3, access)
+
+                stdscr.getch()
             stdscr.clear()
-            stdscr.addstr(0, 0, "Pressed {}".format(menu[highlight]))
             stdscr.refresh()
             # stdscr.getch()
-        print_menu(stdscr, highlight)
+        print_menu(stdscr, highlight, User.get_username(access), access)
         stdscr.refresh()
 
 
